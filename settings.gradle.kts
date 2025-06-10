06abb052b1f0babed0af22994e16ebf13f36b32d
runner <runner@hmpps-github-actions-runner-9979fc9dc-4vj6w>
2025-06-10 09:06:34 +0000

    Rename project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "hmpps-template-kotlin"
+rootProject.name = "hmpps-accredited-programmes-manage-and-deliver-api"
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/AccreditedProgrammesManageAndDeliverApi.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccreditedProgrammesManageAndDeliverApi
+
+fun main(args: Array<String>) {
+  runApplication<AccreditedProgrammesManageAndDeliverApi>(*args)
+}
