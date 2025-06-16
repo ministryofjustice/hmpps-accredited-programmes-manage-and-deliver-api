07b01287dc576595d7fc10fff0401c1fd09ddbca
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-06-16 15:01:34 +0100

    [APG-963] Initial project configuration (#1)
    * [APG-963] Initial project configuration
    
    * [APG-963] add postgres set up for tests in pipeline.yml
    
    * [APG-963] pipeline update
    
    * [APG-963] pipeline fixes for kotlin_validate job
    
    * [APG-963] switch pipeline workflow to gradle_postgres_verify
    
    * [APG-963] add postgres credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,9 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.6")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.5")
@@ -17,6 +18,12 @@ dependencies {
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.29") {
     exclude(group = "io.swagger.core.v3")
   }
+  testImplementation("org.testcontainers:testcontainers:1.21.1")
+  testImplementation("org.testcontainers:postgresql:1.21.1")
+  testImplementation("org.testcontainers:junit-jupiter:1.21.1")
+
+  runtimeOnly("org.flywaydb:flyway-database-postgresql")
+  runtimeOnly("org.postgresql:postgresql:42.7.7")
 }
 
 kotlin {
