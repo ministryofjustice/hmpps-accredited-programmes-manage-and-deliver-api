e39ba7bfa93363f4affac5c74d36501f2bfcc356
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-06-19 10:17:15 +0100

    [APG-1041] Add PreAuth tag

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/HelloWorldController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/HelloWorldController.kt
@@ -1,11 +1,13 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.controller
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloWorldController {
 
+  @PreAuthorize("permitAll()")
   @GetMapping("/hello-world", produces = ["text/plain"])
   fun helloWorld() = "Hello World"
 }
