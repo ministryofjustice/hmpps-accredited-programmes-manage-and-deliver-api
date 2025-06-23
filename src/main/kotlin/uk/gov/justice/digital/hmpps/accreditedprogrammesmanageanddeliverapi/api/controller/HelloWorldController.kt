52fc82004ab38e7abac0773a66b0fa68fc7011c3
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-06-23 16:07:09 +0100

    [APG-1041] Update HelloWorldController to return JSON instead of plain text (#7)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/HelloWorldController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/HelloWorldController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.controller
 
+import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 class HelloWorldController {
 
   @PreAuthorize("hasAnyRole('ROLE_ACCREDITED_PROGRAMMES_MANAGE_AND_DELIVER_API__ACPMAD_UI_WR')")
-  @GetMapping("/hello-world", produces = ["text/plain"])
+  @GetMapping("/hello-world", produces = [MediaType.APPLICATION_JSON_VALUE])
   fun helloWorld() = "Hello World"
 }
