28223068bf68695d59252bb8416f91f993a819d9
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-07-07 08:53:58 +0100

    APG-959/create-find-and-refer-service-client (#23)
    * [APG-959] Integrate Find And Refer API client, add base HMPPS client, and update configuration for external service interaction
    
    * [APG-959] Rename Find And Refer API to Find And Refer Intervention API across the codebase and update related configuration
    
    * [APG-959] Update references and configuration to rename Find And Refer API to Find And Refer Intervention API across the codebase

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/common/exception/ServiceUnavailableException.kt
@@ -0,0 +1,3 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.exception
+
+class ServiceUnavailableException(message: String?, cause: Throwable? = null) : RuntimeException(message, cause)
