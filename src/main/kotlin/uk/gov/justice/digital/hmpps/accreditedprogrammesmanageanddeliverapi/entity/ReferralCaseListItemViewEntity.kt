4a1954ff050c8a78552c2181a6b2c367c06b77ca
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2025-07-14 14:16:59 +0100

    APG-1107: Add caselist view materialised view (#34)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/entity/ReferralCaseListItemViewEntity.kt
@@ -0,0 +1,26 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.validation.constraints.NotNull
+import org.hibernate.annotations.Immutable
+
+@Entity
+@Immutable
+@Table(name = "referral_caselist_item_view")
+class ReferralCaseListItemViewEntity(
+  @NotNull
+  @Id
+  @Column(name = "crn")
+  var crn: String,
+
+  @NotNull
+  @Column(name = "person_name")
+  var personName: String,
+
+  @NotNull
+  @Column(name = "status")
+  var status: String,
+)
