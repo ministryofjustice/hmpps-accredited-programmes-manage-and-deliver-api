800899d777bd4292e39db53c0d478eb963919fc6
Jack Dempster <jack.dempster@justice.gov.uk>
2025-07-16 16:25:43 +0100

    APG-1101: Implement search by crn or personName filter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/CaseListController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/CaseListController.kt
@@ -13,16 +13,17 @@ import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.ReferralCaseListItem
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.service.ReferralCaseListItemService
 
 @PreAuthorize("hasAnyRole('ROLE_ACCREDITED_PROGRAMMES_MANAGE_AND_DELIVER_API__ACPMAD_UI_WR')")
+@RestController
 @Tag(
   name = "Caselist",
   description = "The endpoint fetches the referrals details for the case list view",
 )
-@RestController
 class CaseListController(private val referralCaseListItemService: ReferralCaseListItemService) {
   @Operation(
     tags = ["Caselist"],
@@ -38,13 +39,14 @@ class CaseListController(private val referralCaseListItemService: ReferralCaseLi
     security = [SecurityRequirement(name = "bearerAuth")],
   )
   @GetMapping("/pages/caselist/{openOrClosed}", produces = [MediaType.APPLICATION_JSON_VALUE])
-  fun getOpenCaseListReferrals(
+  fun getCaseListReferrals(
     @PageableDefault(page = 0, size = 10, sort = ["personName"]) pageable: Pageable,
     @PathVariable(
       name = "openOrClosed",
       required = true,
     ) openOrClosed: OpenOrClosed,
-  ): Page<ReferralCaseListItem> = referralCaseListItemService.getReferralCaseListItemServiceByCriteria(pageable, openOrClosed)
+    @RequestParam(name = "crnOrPersonName", required = false) crnOrPersonName: String?,
+  ): Page<ReferralCaseListItem> = referralCaseListItemService.getReferralCaseListItemServiceByCriteria(pageable, openOrClosed, crnOrPersonName)
 }
 
 enum class OpenOrClosed {
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/specification/GetReferralCaseListItemSpecification.kt
@@ -0,0 +1,34 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.specification
+
+import jakarta.persistence.criteria.CriteriaBuilder
+import jakarta.persistence.criteria.CriteriaQuery
+import jakarta.persistence.criteria.Predicate
+import jakarta.persistence.criteria.Root
+import org.springframework.data.jpa.domain.Specification
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.controller.OpenOrClosed
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralCaseListItemViewEntity
+
+fun getReferralCaseListItemSpecification(
+  openOrClosed: OpenOrClosed,
+  crnOrPersonName: String? = null,
+): Specification<ReferralCaseListItemViewEntity> = Specification<ReferralCaseListItemViewEntity> { root: Root<ReferralCaseListItemViewEntity?>, query: CriteriaQuery<*>?, criteriaBuilder: CriteriaBuilder ->
+  val predicates: MutableList<Predicate> = mutableListOf()
+
+  crnOrPersonName?.let {
+    predicates.add(
+      criteriaBuilder.or(
+        criteriaBuilder.like(
+          criteriaBuilder.lower(root.get("personName")),
+          "%$crnOrPersonName%".lowercase(),
+        ),
+        criteriaBuilder.like(
+          criteriaBuilder.lower(root.get("crn")),
+          "%$crnOrPersonName%".lowercase(),
+        ),
+      ),
+
+    )
+  }
+  query?.distinct(true)
+  criteriaBuilder.and(*predicates.toTypedArray())
+}
