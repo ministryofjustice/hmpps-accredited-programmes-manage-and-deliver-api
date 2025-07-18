397e1056c1a8cb2657b2289cfa74e39976dc0f97
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-07-18 12:27:20 +0100

    [APG-1079] Update Helm values for Manage and Deliver API client placeholders and allow SQS message deserialization to ignore unknown properties (#50)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/listener/model/SQSMessage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/listener/model/SQSMessage.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.listener.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class SQSMessage(
   @JsonProperty("Message") val message: String,
 )
