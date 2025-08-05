b63581cda99214228f76e4105b66da5f298fb6d0
ian-moj <ian.johnson@justice.gov.uk>
2025-08-05 14:42:45 +0100

    chore: Cleaning up code further

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/CohortService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/CohortService.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.ser
 
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.OffenceCohort
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.PniScore
 
 @Service
 class CohortService(
@@ -9,28 +10,36 @@ class CohortService(
 ) {
   companion object {
     private const val SEX_DOMAIN_MINIMUM_THRESHOLD = 0.0
-    private const val SEXUAL_OFFENCE_OSP_MINIMUM_THRESHOLD = 0.0
+    private const val OSP_SEXUAL_OFFENCE_MINIMUM_THRESHOLD = 0.0
   }
 
   fun determineOffenceCohort(crn: String): OffenceCohort {
     val pniScore = pniService.getPniScore(crn)
 
-    val ospDcScore = pniScore.riskScore.individualRiskScores.ospDc?.toDoubleOrNull()
-    val ospIicScore = pniScore.riskScore.individualRiskScores.ospIic?.toDoubleOrNull()
+    // Business rule 1: OSP score indicates a sexual offence risk
+    val hasSexualOffenceByOsp = hasSignificantOspScore(pniScore)
 
-    // Business rule 1: OSP >= Low
-    val isSexualOffenceByOsp = listOfNotNull(ospDcScore, ospIicScore).any { it > SEXUAL_OFFENCE_OSP_MINIMUM_THRESHOLD }
+    // Business rule 2: PNI sex domain score indicates a sexual offence risk
+    val hasSexualOffenceByPni = hasSignificantSexDomainScore(pniScore)
 
-    // Business rule 2: Any sexual domain score > 0
-    val isSexualOffenceByPni = with(pniScore.domainScores.sexDomainScore.individualSexScores) {
-      listOfNotNull(sexualPreOccupation, offenceRelatedSexualInterests, emotionalCongruence)
-        .any { it > SEX_DOMAIN_MINIMUM_THRESHOLD }
-    }
-
-    return if (isSexualOffenceByOsp || isSexualOffenceByPni) {
+    return if (hasSexualOffenceByOsp || hasSexualOffenceByPni) {
       OffenceCohort.SEXUAL_OFFENCE
     } else {
       OffenceCohort.GENERAL_OFFENCE
+    }
+  }
+
+  private fun hasSignificantOspScore(pniScore: PniScore): Boolean {
+    val ospDcScore = pniScore.riskScore.individualRiskScores.ospDc?.toDoubleOrNull()
+    val ospIicScore = pniScore.riskScore.individualRiskScores.ospIic?.toDoubleOrNull()
+    return listOfNotNull(ospDcScore, ospIicScore)
+      .any { it > OSP_SEXUAL_OFFENCE_MINIMUM_THRESHOLD }
+  }
+
+  private fun hasSignificantSexDomainScore(pniScore: PniScore): Boolean {
+    return with(pniScore.domainScores.sexDomainScore.individualSexScores) {
+      listOfNotNull(sexualPreOccupation, offenceRelatedSexualInterests, emotionalCongruence)
+        .any { it > SEX_DOMAIN_MINIMUM_THRESHOLD }
     }
   }
 }
