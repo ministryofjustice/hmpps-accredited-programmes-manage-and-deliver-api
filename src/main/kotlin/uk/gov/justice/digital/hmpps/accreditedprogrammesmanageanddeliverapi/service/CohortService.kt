23dbad8811f4a6197fc50fa967434af489793ddc
ian-moj <ian.johnson@justice.gov.uk>
2025-08-05 14:41:23 +0100

    chore: Cleaning up method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/CohortService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/CohortService.kt
@@ -7,23 +7,25 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.
 class CohortService(
   private val pniService: PniService,
 ) {
+  companion object {
+    private const val SEX_DOMAIN_MINIMUM_THRESHOLD = 0.0
+    private const val SEXUAL_OFFENCE_OSP_MINIMUM_THRESHOLD = 0.0
+  }
 
   fun determineOffenceCohort(crn: String): OffenceCohort {
     val pniScore = pniService.getPniScore(crn)
-
-    val isSexDomainAboveZero = with(pniScore.domainScores.sexDomainScore.individualSexScores) {
-      listOfNotNull(sexualPreOccupation, offenceRelatedSexualInterests, emotionalCongruence)
-        .any { it > 0 }
-    }
 
     val ospDcScore = pniScore.riskScore.individualRiskScores.ospDc?.toDoubleOrNull()
     val ospIicScore = pniScore.riskScore.individualRiskScores.ospIic?.toDoubleOrNull()
 
     // Business rule 1: OSP >= Low
-    val isSexualOffenceByOsp = listOfNotNull(ospDcScore, ospIicScore).any { it > 0.0 }
+    val isSexualOffenceByOsp = listOfNotNull(ospDcScore, ospIicScore).any { it > SEXUAL_OFFENCE_OSP_MINIMUM_THRESHOLD }
 
     // Business rule 2: Any sexual domain score > 0
-    val isSexualOffenceByPni = isSexDomainAboveZero
+    val isSexualOffenceByPni = with(pniScore.domainScores.sexDomainScore.individualSexScores) {
+      listOfNotNull(sexualPreOccupation, offenceRelatedSexualInterests, emotionalCongruence)
+        .any { it > SEX_DOMAIN_MINIMUM_THRESHOLD }
+    }
 
     return if (isSexualOffenceByOsp || isSexualOffenceByPni) {
       OffenceCohort.SEXUAL_OFFENCE
