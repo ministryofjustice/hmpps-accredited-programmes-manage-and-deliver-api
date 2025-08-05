2e5bf7c6680f671e9172ecb7fcbba1cd511ac5b0
ian-moj <ian.johnson@justice.gov.uk>
2025-08-05 09:13:46 +0100

    chore: Updating cohort determination based on all PNI scores

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/CohortService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/CohortService.kt
@@ -12,7 +12,21 @@ class CohortService(
   fun determineOffenceCohort(crn: String): OffenceCohort {
     val pniScore = pniService.getPniScore(crn)
 
-    return if (pniScore.domainScores.sexDomainScore.overallSexDomainLevel == NeedLevel.HIGH_NEED) {
+    val isSexDomainAboveZero = with(pniScore.domainScores.sexDomainScore.individualSexScores) {
+      listOfNotNull(sexualPreOccupation, offenceRelatedSexualInterests, emotionalCongruence)
+        .any { it > 0 }
+    }
+
+    val ospDcScore = pniScore.riskScore.individualRiskScores.ospDc?.toDoubleOrNull()
+    val ospIicScore = pniScore.riskScore.individualRiskScores.ospIic?.toDoubleOrNull()
+
+    // Business rule 1: OSP >= Low
+    val isSexualOffenceByOsp = listOfNotNull(ospDcScore, ospIicScore).any { it > 0.0 }
+
+    // Business rule 2: Any sexual domain score > 0
+    val isSexualOffenceByPni = isSexDomainAboveZero
+
+    return if (isSexualOffenceByOsp || isSexualOffenceByPni) {
       OffenceCohort.SEXUAL_OFFENCE
     } else {
       OffenceCohort.GENERAL_OFFENCE
