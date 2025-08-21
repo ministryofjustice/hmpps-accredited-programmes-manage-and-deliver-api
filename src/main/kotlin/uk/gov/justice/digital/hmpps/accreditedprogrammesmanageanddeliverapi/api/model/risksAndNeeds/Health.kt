d19b51f9a5b65a45a9b519c150fd16669e93cb75
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2025-08-21 07:57:43 +0100

    [APG-1137] Adding assessmentId to to health response

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/risksAndNeeds/Health.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/risksAndNeeds/Health.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.risksAndNeeds
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDate
 
 /**
  *
@@ -9,6 +11,10 @@ import io.swagger.v3.oas.annotations.media.Schema
  * @param description
  */
 data class Health(
+
+  @Schema(example = "1 August 2023")
+  @JsonFormat(pattern = "d MMMM yyyy")
+  @get:JsonProperty("assessmentCompleted") val assessmentCompleted: LocalDate? = null,
 
   @Schema(example = "Yes", description = "Does the person have any health condition. Can be Yes, empty or null")
   @get:JsonProperty("anyHealthConditions") val anyHealthConditions: Boolean? = false,
