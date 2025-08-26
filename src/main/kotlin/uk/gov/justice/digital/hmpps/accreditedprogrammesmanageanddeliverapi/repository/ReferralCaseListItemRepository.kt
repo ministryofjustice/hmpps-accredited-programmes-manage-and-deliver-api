9d70bf6cffdbd9f6df730f25ed16121450939a4d
ian-moj <ian.johnson@justice.gov.uk>
2025-08-26 11:27:42 +0100

    chore: Fixing wildcard import

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/ReferralCaseListItemRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/ReferralCaseListItemRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralCaseListItemViewEntity
-import java.util.*
+import java.util.UUID
 
 @Repository
 interface ReferralCaseListItemRepository :
