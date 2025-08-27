79dfce3b73f39cadeafccdcb5a0c3c7e599758a4
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2025-08-27 11:54:49 +0100

    APG-1213(repositories): Remove manual queries for (unused) repo methods on pdu, office, region

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/OfficeRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/OfficeRepository.kt
@@ -1,13 +1,8 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.OfficeEntity
 
 @Repository
-interface OfficeRepository : JpaRepository<OfficeEntity, String> {
-
-  @Query("SELECT o FROM OfficeEntity o WHERE o.deletedAt IS NULL")
-  fun findAllActive(): List<OfficeEntity>
-}
+interface OfficeRepository : JpaRepository<OfficeEntity, String>
