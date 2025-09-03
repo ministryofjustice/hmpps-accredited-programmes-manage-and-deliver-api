b253597ae1a5a6cbf2ffcfd2dd0b42f7cc116649
jack.dempster <jack.dempster@justice.gov.uk>
2025-09-03 11:00:47 +0100

    remove integration test annotation as this will try start test containers instance

--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/integration/InitialiseDatabase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/integration/InitialiseDatabase.kt
@@ -1,8 +1,16 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.integration
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.integration.wiremock.HmppsAuthApiExtension
 
-class InitialiseDatabase : IntegrationTestBase() {
+@ExtendWith(HmppsAuthApiExtension::class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
+class InitialiseDatabase {
 
   // This is needed to initialise the database for schema spy
   @Test
