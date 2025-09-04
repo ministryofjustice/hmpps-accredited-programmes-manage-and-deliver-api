690994864e2e5605dfd26749aec608e2596dcde3
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2025-09-04 14:15:49 +0100

    APG-1222: Create the ReferralService::attemptToFindManagerForReferral method
    **WHAT**
    
    This commit introduces a new method into the ReferralService, which first attempts to find the Manager for a Requirement, and then attempts to find it for the Licence, associated with a Referral.
    
    **WHY**
    
    In order to deliver a set of Delivery Locations to a user, associated with a Referral, we need to search based on the Case, associated with the Person on Probation and the upstream "sourced from" event, i.e. a Licence Condition or a Requirement.
    
    Although we _may_ have some knowledge about if our event was sourced from a Requirement or Referral, this method assumes a naive system, that has to check both.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ReferralService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ReferralService.kt
@@ -9,6 +9,8 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.clie
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.findAndReferInterventionApi.FindAndReferInterventionApiClient
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.findAndReferInterventionApi.model.FindAndReferReferralDetails
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.findAndReferInterventionApi.model.toReferralEntity
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.nDeliusIntegrationApi.NDeliusIntegrationApiClient
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.nDeliusIntegrationApi.model.RequirementOrLicenceConditionManager
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralStatusHistoryEntity
@@ -20,6 +22,7 @@ import java.util.*
 @Transactional
 open class ReferralService(
   private val findAndReferInterventionApiClient: FindAndReferInterventionApiClient,
+  private val ndeliusIntegrationApiClient: NDeliusIntegrationApiClient,
   private val referralRepository: ReferralRepository,
   private val serviceUserService: ServiceUserService,
   private val cohortService: CohortService,
@@ -63,4 +66,51 @@ open class ReferralService(
   }
 
   fun getReferralById(id: UUID): ReferralEntity? = referralRepository.findByIdOrNull(id)
+
+  fun attemptToFindManagerForReferral(referralId: UUID): RequirementOrLicenceConditionManager? {
+    val referral = getReferralById(referralId)
+
+    if (referral == null) {
+      throw NotFoundException("No Referral found for id: $referralId")
+    } else if (referral.eventId.isNullOrEmpty()) {
+      throw NotFoundException("Referral with id: $referralId exists, but has no eventId")
+    }
+
+    val referralIdString = referral.id.toString()
+    val eventId: String = referral.eventId!!
+
+    log.info("Attempting to retrieve a Requirement for Referral with ID: $referralId...")
+    // TODO: Check to see if the sourced_from field is set, then make an informed opinion based on that --TJWC 2025-09-04
+
+    when (val response = ndeliusIntegrationApiClient.getRequirementManagerDetails(referralIdString, eventId)) {
+      is ClientResult.Success -> {
+        log.info("...success! Found a Requirement for referral with ID: $referralId")
+        return response.body.manager
+      }
+      is ClientResult.Failure.StatusCode -> {
+        if (response.status.value() == 404) {
+          log.info("...could not find Requirement for Referral with ID: $referralId")
+        }
+      }
+      else -> {
+        log.error("...failure, encountered an error while fetching Requirement for Referral with ID: $referralId from nDelius Integration API")
+        throw NotFoundException("Could not fetch a Requirement with ID $eventId, for Referral with ID: $referralId")
+      }
+    }
+
+    log.info("...attempting to retrieve a Licence Condition for Referral with ID: $referralId")
+
+    when (val response = ndeliusIntegrationApiClient.getLicenceConditionManagerDetails(referralIdString, eventId)) {
+      is ClientResult.Success -> {
+        log.info("...success! Found a Licence Condition for referral with ID: $referralId")
+        return response.body.manager
+      }
+      else -> {
+        log.error("...failure, neither a Requirement or Licence Condition returned for Referral with ID $referralId")
+        return null
+      }
+    }
+
+    return null
+  }
 }
