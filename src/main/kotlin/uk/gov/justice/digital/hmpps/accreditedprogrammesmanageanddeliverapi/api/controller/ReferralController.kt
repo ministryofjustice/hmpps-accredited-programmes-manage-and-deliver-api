a94e7130827fe3efb06f0d539c0972e4635f8ac0
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2025-09-15 16:58:38 +0100

    APG-1169: GET delivery location preferences return empty when no preferences set (#159)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ReferralController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ReferralController.kt
@@ -26,6 +26,7 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.nDeliusIntegrationApi.model.RequirementOrLicenceConditionManager
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.exception.BusinessException
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.exception.NotFoundException
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.service.DeliveryLocationPreferencesService
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.service.OffenceService
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.service.ReferralService
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.service.SentenceService
@@ -39,6 +40,7 @@ class ReferralController(
   private val serviceUserService: ServiceUserService,
   private val offenceService: OffenceService,
   private val sentenceService: SentenceService,
+  private val deliveryLocationPreferencesService: DeliveryLocationPreferencesService,
 ) {
 
   private val log = LoggerFactory.getLogger(this::class.java)
@@ -242,7 +244,10 @@ class ReferralController(
   )
   @PutMapping("/referral/{id}/update-cohort", produces = [MediaType.APPLICATION_JSON_VALUE])
   fun updateCohortForReferral(
-    @Parameter(description = "The id (UUID) of a referral allowed values SEXUAL_OFFENCE or GENERAL_OFFENCE", required = true)
+    @Parameter(
+      description = "The id (UUID) of a referral allowed values SEXUAL_OFFENCE or GENERAL_OFFENCE",
+      required = true,
+    )
     @PathVariable("id") id: UUID,
     @Parameter(
       description = "Cohort to update the referral with",
@@ -328,8 +333,7 @@ class ReferralController(
     @Parameter(description = "The id (UUID) of a referral", required = true)
     @PathVariable("id") id: UUID,
   ): ResponseEntity<DeliveryLocationPreferences> {
-    val referral = referralService.getReferralById(id) ?: throw NotFoundException("Referral with id $id not found")
-    return referral.deliveryLocationPreferences?.toModel()?.let { ResponseEntity.ok(it) }
-      ?: throw NotFoundException("Delivery location preferences for referral id $id not found")
+    val deliveryLocationPreferences = deliveryLocationPreferencesService.getPreferredDeliveryLocationsForReferral(id)
+    return ResponseEntity.ok().body(deliveryLocationPreferences)
   }
 }
