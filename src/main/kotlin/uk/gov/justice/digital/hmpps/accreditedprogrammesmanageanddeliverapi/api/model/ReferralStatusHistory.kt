90711100cc1df7fe10462d07ecd328787f3bd0e4
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2025-09-17 15:03:55 +0100

    APG-1339: Create the POST /referral/:id/status-history endpoint
    **WHAT**
    
    This commit creates a new endpoint at `POST /referral/:id/status-history` which allows a user to update the Referral Status of a Referral by creating a new entry in the Referral Status History table
    
    **WHY**
    
    This endpoint will eventually be used by the UI in order to update the
    Referal Status.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/ReferralStatusHistory.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class ReferralStatusHistory(
+  @Schema(
+    example = "21da0995-5827-4cc9-bbc9-c7c7f2975163",
+    required = true,
+    description = "ID of the ReferralStatusHistory entry",
+  )
+  @get:JsonProperty("id", required = true)
+  val id: String,
+
+  @Schema(
+    example = "21da0995-5827-4cc9-bbc9-c7c7f2975163",
+    required = true,
+    description = "ID of the Referral Status Description",
+  )
+  @get:JsonProperty("referralStatusDescriptionId", required = true)
+  val referralStatusDescriptionId: String,
+
+  @Schema(
+    example = "Awaiting assessment",
+    required = true,
+    description = "Human-readable name of the Referral Status, useful for UIs",
+  )
+  @get:JsonProperty("referralStatusDescriptionName", required = true)
+  val referralStatusDescriptionName: String,
+)
