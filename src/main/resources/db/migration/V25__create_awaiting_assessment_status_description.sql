5af2390335079f8046819127090b7694e4e177ac
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2025-09-17 13:49:57 +0100

    migrations: remove duplicates, and fix the order of all the migrations

--- src/main/resources/db/migration/V25__create_awaiting_assessment_status_description.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO referral_status_description
-    (id, description_text,
-     is_closed,
-     label_colour,
-     created_at,
-     updated_at, deleted_at)
-VALUES ('76b2f8d8-260c-4766-a716-de9325292609', 'Awaiting Assessment', false, null, now(), now(), null);
--- src/main/resources/db/migration/V27__update_referral_status_history_table.sql
+++ src/main/resources/db/migration/V27__update_referral_status_history_table.sql
@@ -4,9 +4,6 @@ DROP TABLE referral_status_history_mapping;
 
 -- Then we have to add these columns to the table, one-by-one
 ALTER TABLE referral_status_history
-    ADD COLUMN referral_status_description_id UUID REFERENCES referral_status_description (id);
-
-ALTER TABLE referral_status_history
     ADD COLUMN referral_id UUID REFERENCES referral (id);
 
 ALTER TABLE referral_status_history
@@ -37,8 +34,6 @@ CREATE INDEX idx_referral_person_name ON referral_caselist_item_view (person_nam
 CREATE INDEX idx_referral_status ON referral_caselist_item_view (status);
 
 -- Finally, create some indexes on the new rows, for performance
-CREATE INDEX idx_referral_status_history_description_id ON referral_status_history (referral_status_description_id);
 CREATE INDEX idx_referral_status_history_referral_id ON referral_status_history (referral_id);
 
-COMMENT ON COLUMN referral_status_history.referral_status_description_id IS 'References the status description for this history entry';
 COMMENT ON COLUMN referral_status_history.referral_id IS 'References the Referral that this Status History belongs to';
