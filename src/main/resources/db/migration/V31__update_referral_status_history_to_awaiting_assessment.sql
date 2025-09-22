8b08425b70f9e8fe9dedaebcd6b188fcd48baa19
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-09-22 10:10:10 +0100

    [APG-1340] Recreate and populate referral status history with "Awaiting assessment" data

--- src/main/resources/db/migration/V31__update_referral_status_history_to_awaiting_assessment.sql
+++ src/main/resources/db/migration/V31__update_referral_status_history_to_awaiting_assessment.sql
@@ -1,2 +1,11 @@
-UPDATE referral_status_history
-SET referral_status_description_id = (SELECT id FROM referral_status_description WHERE description_text = 'Awaiting assessment');
+DELETE FROM referral_status_history;
+
+INSERT INTO referral_status_history (id, created_at, created_by, start_date, referral_status_description_id, referral_id)
+SELECT
+    gen_random_uuid(),
+    NOW(),
+    'SYSTEM',
+    NOW(),
+    (SELECT id FROM referral_status_description WHERE description_text = 'Awaiting assessment'),
+    r.id
+FROM referral r;
