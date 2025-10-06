dc9f37386cf6b99ce0ee2b3bef8681bf33a5edb5
Ryan Forsyth <ryan.forsyth@justice.gov.uk>
2025-10-06 11:25:12 +0100

    return reporting locations for case list filters

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/ReferralReportingLocationRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/ReferralReportingLocationRepository.kt
@@ -1,10 +1,16 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.caseList.PduReportingLocation
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralReportingLocationEntity
 import java.util.UUID
 
 interface ReferralReportingLocationRepository : JpaRepository<ReferralReportingLocationEntity, ReferralEntity> {
   fun findByReferralId(referralId: UUID?): ReferralReportingLocationEntity?
+  fun findFirstByReferralId(referralId: UUID): ReferralReportingLocationEntity?
+
+  @Query("SELECT r.pduName, r.reportingTeam FROM ReferralReportingLocationEntity r GROUP BY r.pduName, r.reportingTeam")
+  fun getPdusAndReportingTeams(): List<PduReportingLocation>
 }
