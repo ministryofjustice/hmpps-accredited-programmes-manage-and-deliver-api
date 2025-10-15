61218e0de1bd23e675578d55889ec3d5ea9d0034
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2025-10-15 07:40:18 +0100

    chore: remove migrations 1-9 without the leading 0s

--- src/main/resources/db/migration/V1__create_message_history_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE message_history (
-    id UUID NOT NULL PRIMARY KEY,
-    event_type TEXT NOT NULL,
-    detail_url TEXT NOT NULL,
-    description TEXT,
-    occurred_at TIMESTAMP NOT NULL,
-    message TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL
-);
--- src/main/resources/db/migration/V5__add_columns_to_referral.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE referral
-ADD COLUMN intervention_type TEXT,
-ADD COLUMN intervention_name TEXT,
-ADD COLUMN setting TEXT;
-
