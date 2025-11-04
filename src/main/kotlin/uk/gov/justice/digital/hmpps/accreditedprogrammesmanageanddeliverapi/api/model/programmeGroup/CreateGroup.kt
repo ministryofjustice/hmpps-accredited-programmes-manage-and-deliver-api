5d0b51feba289c84a536cdbcc3b020a81606f7fb
jack.dempster <jack.dempster@justice.gov.uk>
2025-11-04 09:17:24 +0000

    APG-1433: Add swagger documentation for enums

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/programmeGroup/CreateGroup.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/programmeGroup/CreateGroup.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.programmeGroup
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.ProgrammeGroupCohort
@@ -13,9 +14,19 @@ data class CreateGroup(
   var groupCode: String,
   @NotNull(message = "cohort must not be null")
   @get:JsonProperty("cohort", required = true)
+  @Schema(
+    enumAsRef = true,
+    description = "Cohort for the Programme Group.",
+    implementation = ProgrammeGroupCohort::class,
+  )
   var cohort: ProgrammeGroupCohort,
   @NotNull(message = "sex must not be null")
   @get:JsonProperty("sex", required = true)
+  @Schema(
+    enumAsRef = true,
+    description = "Sex that the group is being run for",
+    implementation = ProgrammeGroupSex::class,
+  )
   var sex: ProgrammeGroupSex,
 )
 
