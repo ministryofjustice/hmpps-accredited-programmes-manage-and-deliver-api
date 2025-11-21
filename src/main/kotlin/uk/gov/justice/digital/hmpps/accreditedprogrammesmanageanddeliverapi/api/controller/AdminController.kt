bd7e9ddcb57d9e5b64f54f8ba1ea4bba0e533fc9
jack.dempster <jack.dempster@justice.gov.uk>
2025-11-21 16:49:15 +0000

    WIP: Delete referrals where we can find no delius or sentence inforation for the referral

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/AdminController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/AdminController.kt
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -100,5 +101,10 @@ class AdminController(
     }
 
     return ResponseEntity.ok(PopulatePersonalDetailsResponse(ids = request.referralIds))
+  }
+
+  @GetMapping("/admin/check-referral-info")
+  suspend fun checkIfReferralHasInfo() {
+    adminService.cleanUpReferralsWithNoDeliusOrOasysData()
   }
 }
