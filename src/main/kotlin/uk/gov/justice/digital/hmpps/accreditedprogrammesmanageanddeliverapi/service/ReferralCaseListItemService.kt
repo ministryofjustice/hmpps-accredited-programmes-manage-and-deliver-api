4880ca62f6694512d98dd0b6d4edaebc587c6e5b
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-12-02 13:53:50 +0000

    [APG-1466] Refactor referral case list filtering to use both cohort and LDC (#321)
    * [APG-1466] Refactor referral case list filtering to use `OffenceCohort` and `hasLdc` for advanced cohort filtering (#327)
    
    * [APG-1466] Reinstate region-based filtering for referral case lists and handle missing regions gracefully

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ReferralCaseListItemService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ReferralCaseListItemService.kt
@@ -7,15 +7,18 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.controller.OpenOrClosed
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.LocationFilterValues
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.OffenceCohort
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.caseList.CaseListFilterValues
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.caseList.CaseListReferrals
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.caseList.StatusFilterValues
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.caseList.toApi
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.programmeGroup.ProgrammeGroupCohort
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralCaseListItemViewEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.ReferralCaseListItemRepository
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.ReferralReportingLocationRepository
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.specification.getReferralCaseListItemSpecification
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.specification.withAllowedCrns
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.specification.withRegionNames
 
 @Service
 class ReferralCaseListItemService(
@@ -31,17 +34,21 @@ class ReferralCaseListItemService(
     openOrClosed: OpenOrClosed,
     username: String,
     crnOrPersonName: String?,
-    cohort: String?,
+    cohort: ProgrammeGroupCohort?,
     status: String?,
     pdu: String?,
     reportingTeams: List<String>?,
   ): CaseListReferrals {
+    val (offenceType, hasLdc) = cohort?.let { ProgrammeGroupCohort.toOffenceTypeAndLdc(it) }
+      ?: (null to null)
+
     val referralsToReturn = getReferralCaseList(
       pageable = pageable,
       openOrClosed = openOrClosed,
       username = username,
       crnOrPersonName = crnOrPersonName,
-      cohort = cohort,
+      offenceCohort = offenceType,
+      hasLdc = hasLdc,
       status = status,
       pdu = pdu,
       reportingTeams = reportingTeams,
@@ -52,7 +59,8 @@ class ReferralCaseListItemService(
       openOrClosed = if (openOrClosed == OpenOrClosed.OPEN) OpenOrClosed.CLOSED else OpenOrClosed.OPEN,
       username = username,
       crnOrPersonName = crnOrPersonName,
-      cohort = cohort,
+      offenceCohort = offenceType,
+      hasLdc = hasLdc,
       status = status,
       pdu = pdu,
       reportingTeams = reportingTeams,
@@ -66,26 +74,34 @@ class ReferralCaseListItemService(
     openOrClosed: OpenOrClosed,
     username: String,
     crnOrPersonName: String?,
-    cohort: String?,
+    offenceCohort: OffenceCohort?,
+    hasLdc: Boolean?,
     status: String?,
     pdu: String?,
     reportingTeams: List<String>?,
   ): Page<ReferralCaseListItemViewEntity> {
-//    val userRegions = userService.getUserRegions(username)
-
     val possibleStatuses = referralStatusService.getOpenOrClosedStatusesDescriptions(openOrClosed)
 
     val baseSpec =
-      getReferralCaseListItemSpecification(possibleStatuses, crnOrPersonName, cohort, status, pdu, reportingTeams)
+      getReferralCaseListItemSpecification(
+        possibleStatuses = possibleStatuses,
+        crnOrPersonName = crnOrPersonName,
+        offenceCohort = offenceCohort,
+        hasLdc = hasLdc,
+        status = status,
+        pdu = pdu,
+        reportingTeams = reportingTeams,
+      )
 
-//    val specWithRegions = if (userRegions.isEmpty()) {
-//      log.warn("No regions found for user: $username. Returning empty list for ReferralCaseList.")
-//      return PageImpl(emptyList(), pageable, 0)
-//    } else {
-//      withRegionNames(baseSpec, userRegions)
-//    }
+    val userRegions = userService.getUserRegions(username).map { it.description }
+    val specWithRegions = if (userRegions.isEmpty()) {
+      log.warn("No regions found for user: $username. Returning empty list for ReferralCaseList.")
+      return PageImpl(emptyList(), pageable, 0)
+    } else {
+      withRegionNames(baseSpec, userRegions)
+    }
 
-    val crns = referralCaseListItemRepository.findAllCrns(baseSpec)
+    val crns = referralCaseListItemRepository.findAllCrns(specWithRegions)
 
     if (crns.isEmpty()) {
       log.warn("No CRNs found for user: $username. Returning empty list for ReferralCaseList.")
@@ -99,7 +115,7 @@ class ReferralCaseListItemService(
       return PageImpl(emptyList(), pageable, 0)
     }
 
-    val restrictedSpec = withAllowedCrns(baseSpec, allowedCrns)
+    val restrictedSpec = withAllowedCrns(specWithRegions, allowedCrns)
     return referralCaseListItemRepository.findAll(restrictedSpec, pageable)
   }
 
