3e9e7b733e4cf7a63092f6cfe8ac3cea46254fe0
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2025-12-03 14:37:18 +0000

    Add Session, SessionAttendance, and NDeliusAppointment entities (#325)
    * [APG-1580] Add Session, SessionAttendance, and NDeliusAppointment entities with corresponding Flyway migration script
    
    * [APG-1589] Extend TestDataCleaner with truncation for NDeliusAppointment, SessionAttendance and Session tables

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/entity/SessionEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/entity/SessionEntity.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
@@ -10,15 +13,15 @@ import jakarta.persistence.ManyToOne
 import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotNull
-import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.security.core.context.SecurityContextHolder
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.type.SessionType
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Entity
-@Table(name = "programme_group_membership")
-class ProgrammeGroupMembershipEntity(
+@Table(name = "session")
+class SessionEntity(
   @Id
   @GeneratedValue
   @Column(name = "id")
@@ -26,33 +29,49 @@ class ProgrammeGroupMembershipEntity(
 
   @NotNull
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "referral_id")
-  var referral: ReferralEntity,
-
-  @NotNull
-  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "programme_group_id")
   var programmeGroup: ProgrammeGroupEntity,
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "module_session_template_id")
+  var moduleSessionTemplate: ModuleSessionTemplateEntity? = null,
+
+  @NotNull
+  @Column(name = "sequence_number")
+  var sequenceNumber: Int,
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  @Column(name = "session_type")
+  var sessionType: SessionType,
+
+  @NotNull
+  @Column(name = "is_catchup")
+  var isCatchup: Boolean = false,
+
+  @Column(name = "location_name")
+  var locationName: String? = null,
+
+  @NotNull
+  @Column(name = "starts_at")
+  var startsAt: LocalDateTime,
+
+  @NotNull
+  @Column(name = "ends_at")
+  var endsAt: LocalDateTime,
 
   @NotNull
   @Column(name = "created_at")
   @CreatedDate
   var createdAt: LocalDateTime = LocalDateTime.now(),
 
-  @NotNull
   @Column(name = "created_by_username")
-  @CreatedBy
   var createdByUsername: String? = SecurityContextHolder.getContext().authentication?.name ?: "UNKNOWN_USER",
-
-  @Column(name = "deleted_at")
-  var deletedAt: LocalDateTime? = null,
-
-  @Column(name = "deleted_by_username")
-  var deletedByUsername: String? = null,
 
   @OneToMany(
     fetch = FetchType.LAZY,
-    mappedBy = "groupMembership",
+    cascade = [CascadeType.ALL],
+    mappedBy = "session",
   )
   var attendances: MutableSet<SessionAttendanceEntity> = mutableSetOf(),
 )
