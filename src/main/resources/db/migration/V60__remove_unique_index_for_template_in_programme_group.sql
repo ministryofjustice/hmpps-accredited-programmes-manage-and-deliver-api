7e4620f47a0881ef32052bb4db0cb04e1bb8ae03
jack.dempster <jack.dempster@justice.gov.uk>
2025-12-11 12:08:34 +0000

    Remove unique constraint on template_id in programme group as multiple groups will have same template id

---
+++ src/main/resources/db/migration/V60__remove_unique_index_for_template_in_programme_group.sql
@@ -0,0 +1,3 @@
+DROP INDEX idx_programme_group_accredited_programme_template_id;
+CREATE INDEX IF NOT EXISTS idx_programme_group_accredited_programme_template_id
+    ON programme_group (accredited_programme_template_id);
