e21b86474549157d934dfa4c1473c13114105761
jack.dempster <jack.dempster@justice.gov.uk>
2025-12-11 13:04:49 +0000

    APG-1606: Compute moduleNumber and sessionNumber rather than having additional rows in db

--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/factory/programmeGroup/CreateGroupRequestFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/factory/programmeGroup/CreateGroupRequestFactory.kt
@@ -5,6 +5,7 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.programmeGroup.CreateGroupTeamMember
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.programmeGroup.ProgrammeGroupCohort
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.type.ProgrammeGroupSexEnum
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.asString
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.randomUppercaseString
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.randomWord
 import java.time.LocalDate
@@ -33,7 +34,7 @@ class CreateGroupRequestFactory(
     createGroupSessionSlot = createGroupSessionSlot ?: setOf(sessionSlotFactory.produce()),
     pduName = pduName ?: randomWord(1..2).toString(),
     pduCode = pduCode ?: randomUppercaseString(),
-    deliveryLocationName = deliveryLocationName ?: randomWord(1..2).toString(),
+    deliveryLocationName = deliveryLocationName ?: randomWord(1..3).asString(),
     deliveryLocationCode = deliveryLocationCode ?: randomUppercaseString(),
     teamMembers = teamMembers ?: listOf(teamMemberFactory.produce()),
   )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ScheduleServiceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ScheduleServiceIntegrationTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.service
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -7,9 +8,13 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.clie
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.nDeliusIntegrationApi.model.NDeliusUserTeam
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.nDeliusIntegrationApi.model.NDeliusUserTeams
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.factory.programmeGroup.CreateGroupRequestFactory
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.factory.programmeGroup.CreateGroupSessionSlotFactory
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.AccreditedProgrammeTemplateRepository
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.SessionRepository
+import uk.gov.justice.hmpps.test.kotlin.auth.WithMockAuthUser
 
+@WithMockAuthUser("AUTOMATED_TEST_USER")
 class ScheduleServiceIntegrationTest : IntegrationTestBase() {
   @Autowired
   private lateinit var scheduleService: ScheduleService
@@ -19,6 +24,9 @@ class ScheduleServiceIntegrationTest : IntegrationTestBase() {
 
   @Autowired
   private lateinit var accreditedProgrammeTemplateRepository: AccreditedProgrammeTemplateRepository
+
+  @Autowired
+  private lateinit var sessionRepository: SessionRepository
 
   @BeforeEach
   override fun beforeEach() {
@@ -40,9 +48,14 @@ class ScheduleServiceIntegrationTest : IntegrationTestBase() {
 
   @Test
   fun `should generate sessions for a group based on template`() {
-    val createGroupRequest = CreateGroupRequestFactory().produce()
+    val sessionSlots = CreateGroupSessionSlotFactory().produceUniqueSlots(3)
+    val createGroupRequest = CreateGroupRequestFactory().produce(createGroupSessionSlot = sessionSlots)
     val group = programmeGroupService.createGroup(createGroupRequest, "AUTH_ADM")
 
     scheduleService.scheduleSessionForGroup(group.id!!)
+
+    val sessions = sessionRepository.findAll()
+
+    assertThat(sessions).isNotEmpty
   }
 }
