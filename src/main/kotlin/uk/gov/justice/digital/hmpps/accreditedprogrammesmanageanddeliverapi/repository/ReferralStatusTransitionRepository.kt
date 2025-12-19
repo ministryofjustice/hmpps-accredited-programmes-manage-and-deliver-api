9580ba78873d99d717be634d64d602fa6eb43847
jack.dempster <jack.dempster@justice.gov.uk>
2025-12-19 15:31:01 +0000

    APG-1520 Add visible column to status transitions so we can hide these columns in the ui when displaying status transitions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/ReferralStatusTransitionRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/repository/ReferralStatusTransitionRepository.kt
@@ -1,10 +1,14 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralStatusDescriptionEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralStatusTransitionEntity
 import java.util.UUID
 
 interface ReferralStatusTransitionRepository : JpaRepository<ReferralStatusTransitionEntity, UUID> {
   fun findByFromStatusId(fromStatusId: UUID): List<ReferralStatusTransitionEntity>
   fun findByFromStatusIdAndToStatusId(fromStatusId: UUID, toStatusId: UUID): ReferralStatusTransitionEntity?
+  fun findByFromStatus(fromStatus: ReferralStatusDescriptionEntity): MutableList<ReferralStatusTransitionEntity>
+  fun deleteAllByFromStatus(fromStatus: ReferralStatusDescriptionEntity)
+  fun findByToStatus(toStatus: ReferralStatusDescriptionEntity): MutableList<ReferralStatusTransitionEntity>
 }
