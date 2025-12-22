1ee9fda93243463caf878f5903d67dc0180d2ecd
niaevans9 <nia.evans@digital.justice.gov.uk>
2025-12-22 12:04:37 +0000

    Return a success message as part of the response when updating referral status (#360)
    * Return a success message as part of the response
    
    * Linting

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/StatusUpdateResponse.kt
@@ -0,0 +1,16 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Response returned after updating a referral status")
+data class StatusUpdateResponse(
+  val referralStatusHistory: ReferralStatusHistory,
+  @Schema(
+    example = "Alex River's referral status is now Recall. They have been removed from group BCCDD1'.",
+    required = true,
+    description = "The text to show to the user, confirming the status update has taken place",
+  )
+  @get:JsonProperty("message", required = true)
+  val message: String,
+)
