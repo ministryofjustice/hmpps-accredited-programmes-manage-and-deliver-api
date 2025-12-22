234849ac5f44b9e930fe3a0b325018d0f5e978b2
jack.dempster <jack.dempster@justice.gov.uk>
2025-12-22 14:22:10 +0000

    AGP-1584 Skip bank holidays when scheduling sessions for a group

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/client/govUkHolidaysApi/GovUkApiClient.kt
@@ -0,0 +1,21 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.govUkHolidaysApi
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.BaseHMPPSClient
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.govUkHolidaysApi.model.BankHolidaysResponse
+
+@Component
+class GovUkApiClient(
+  @Qualifier("govUkApiWebClient") webClient: WebClient,
+  objectMapper: ObjectMapper,
+) : BaseHMPPSClient(webClient, objectMapper) {
+  private val log = LoggerFactory.getLogger(this::class.java)
+
+  fun getHolidays() = getRequest<BankHolidaysResponse>("GovUK Bank Holidays API") {
+    path = "/bank-holidays.json"
+  }
+}
