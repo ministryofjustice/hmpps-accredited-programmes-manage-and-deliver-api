ffd565746391ec8332dea8775f42fed4b0921ad9
Ryan Forsyth <ryan.forsyth@justice.gov.uk>
2026-01-06 15:46:07 +0000

    add further comments to migration file

--- src/main/resources/db/migration/V70__consolidate_group_and_one_to_one_modules.sql
+++ src/main/resources/db/migration/V70__consolidate_group_and_one_to_one_modules.sql
@@ -18,12 +18,13 @@ UPDATE module_session_template SET module_id = '78c9d0e1-2a3b-40f6-a163-8d9e0f1a
 UPDATE module SET module_number = '5' WHERE id in ('78c9d0e1-2a3b-40f6-a163-8d9e0f1a2b3c', '89d0e1f2-3a4b-4017-b274-9e0f1a2b3c4d');
 DELETE FROM module WHERE id in ('89d0e1f2-3a4b-4017-b274-9e0f1a2b3c4d');
 
+-- Bringing it all together (update module number)
 UPDATE module SET module_number = '6' WHERE id = '9ad0e1f2-4a5b-4028-c385-0f1a2b3c4d5e';
 
+
+-- new module and associated session for post programme review
 INSERT INTO module (id, accredited_programme_template_id, name, module_number)
 VALUES (uuid 'ac581f6c-1d81-45a2-af1e-7e3a041ae756', uuid '3442732f-9a0d-4981-8f9e-54e622e72211', 'Post Programme Review', 7);
 
 INSERT INTO module_session_template (id, module_id, session_number, session_type, pathway, name, duration_minutes)
-
--- 1) Post Programme Review (3 way meeting) (Individual); Moderate = 1
 Values(uuid 'fb60da35-7de1-4d25-a03d-6ff2c9baec3f', uuid 'ac581f6c-1d81-45a2-af1e-7e3a041ae756', 1, 'ONE_TO_ONE', 'MODERATE_INTENSITY', 'Post Programme Review', 60);
