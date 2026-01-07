0bf8a7b7b8613e51360d2ad5a9d41975cf21c476
jack.dempster <jack.dempster@justice.gov.uk>
2026-01-07 11:21:45 +0000

    APG-1574: Add retry mechanism for govuk api

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/client/govUkHolidaysApi/GovUkApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/client/govUkHolidaysApi/GovUkApiClient.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.govUkHolidaysApi
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.resilience4j.retry.annotation.Retry
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.cache.annotation.Cacheable
@@ -16,6 +17,7 @@ class GovUkApiClient(
 ) : BaseHMPPSClient(webClient, objectMapper) {
   private val log = LoggerFactory.getLogger(this::class.java)
 
+  @Retry(name = "govUkApi")
   @Cacheable("bank-holidays", unless = "#result == null")
   fun getHolidays() = getRequest<BankHolidaysResponse>("GovUk Bank Holidays API") {
     path = "/bank-holidays.json"
