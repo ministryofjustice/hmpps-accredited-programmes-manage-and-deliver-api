0f9ca86c19d7fce539889b2badacd5a26c49d1de
Ryan Forsyth <ryan.forsyth@justice.gov.uk>
2026-01-14 16:43:06 +0000

    update session name depending on session type

--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ProgrammeGroupControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ProgrammeGroupControllerIntegrationTest.kt
@@ -1932,7 +1932,7 @@ class ProgrammeGroupControllerIntegrationTest : IntegrationTestBase() {
       assertThat(response.group).isNotNull
       assertThat(response.modules).isNotNull
       assertThat(response.modules.size).isEqualTo(7)
-      assertThat(response.modules.sumOf { it.sessions.count() }).isEqualTo(27)
+      assertThat(response.modules.sumOf { it.sessions.count() }).isEqualTo(21)
       response.modules.forEach { module ->
         module.sessions.forEach { session ->
           when (session.type) {
