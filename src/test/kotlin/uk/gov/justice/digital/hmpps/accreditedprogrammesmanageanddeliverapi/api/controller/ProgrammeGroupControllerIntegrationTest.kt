30a9f586243f278db93e55eb22958db07ccccf43
Ryan Forsyth <ryan.forsyth@justice.gov.uk>
2026-01-15 16:11:19 +0000

    tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ProgrammeGroupControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ProgrammeGroupControllerIntegrationTest.kt
@@ -1966,11 +1966,10 @@ class ProgrammeGroupControllerIntegrationTest : IntegrationTestBase() {
       response.modules.forEach { module ->
         module.sessions.forEach { session ->
           when (session.type) {
-            SessionType.GROUP -> {
+            "Group" -> {
               assertThat(session.participants).isEqualTo(listOf("All"))
             }
-
-            SessionType.ONE_TO_ONE -> {
+            "Individual" -> {
               assertThat(session.participants).isNotEqualTo(listOf("All")) // TO be updated when attendees/facilitators tables are added.
             }
           }
