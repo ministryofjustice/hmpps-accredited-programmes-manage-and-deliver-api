16dd026f9c745bc1183174ac325c535540c764d1
jack.dempster <jack.dempster@justice.gov.uk>
2026-01-16 15:34:58 +0000

    APG-1557 Allow empty response body to be returned from HTTP requests by deserializing it to an empty object

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/EmptyResponse.kt
@@ -0,0 +1,4 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model
+
+// This is used for deserializing requests that have empty body's such as HTTP 201.
+object EmptyResponse
