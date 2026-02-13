a025445433da17409a107247ba9f2182ab603552
jack.dempster <jack.dempster@justice.gov.uk>
2026-02-13 11:12:26 +0000

    APG-1674 Add tests for FormatSessionNameForPageFunction

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/utils/FormatSessionNameForPage.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/utils/FormatSessionNameForPage.kt
@@ -10,6 +10,7 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.enti
  * Pages this function can be used on (please add to this list if implemented for another page):
  * <ul>
  *   <li>Edit Session Facilitators</li>
+ *   <li>Delete Session</li>
  * </ul>
  *
  * @param session The session entity to format
