d8a550ece1465cb0f23605bd9c86ab1d7d60b488
jack.dempster <jack.dempster@justice.gov.uk>
2026-03-05 10:39:57 +0000

    APG-1520 : Move status change details endpoint into own controller and secure with new role

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/StatusChangeDetailsController.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.controller
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.ReferralStatusInfo
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.service.ReferralStatusService
+import java.util.UUID
+
+@RestController
+@PreAuthorize("hasAnyRole('ACCREDITED_PROGRAMMES__MANAGE_AND_DELIVER__READ_ONLY')")
+class StatusChangeDetailsController(
+  private val referralStatusService: ReferralStatusService,
+) {
+
+  private val log = LoggerFactory.getLogger(this::class.java)
+
+  @GetMapping("/referral/{referralId}/status-change-details")
+  fun getStatusChangeDetails(@PathVariable referralId: UUID): ResponseEntity<ReferralStatusInfo> {
+    log.info("Request to retrieve details of status change for referral with id: $referralId")
+    val statusInfo = referralStatusService.getStatusChangeDetailsForReferral(referralId)
+
+    return ResponseEntity.ok(statusInfo)
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ReferralControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/ReferralControllerIntegrationTest.kt
@@ -16,7 +16,6 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.OffenceHistory
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.PersonalDetails
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.ReferralDetails
-import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.ReferralStatusInfo
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.ReferralStatusTransitions
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.RemoveReferralFromGroupStatusTransitions
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.SentenceInformation
@@ -1258,32 +1257,6 @@ class ReferralControllerIntegrationTest(@Autowired private val programmeGroupMem
         "Recall",
         "Return to court",
       )
-    }
-  }
-
-  @Nested
-  @DisplayName("Get the details of a status change for a referral")
-  inner class GetStatusChangeForReferral {
-    @Test
-    fun `should return 200 response and ReferralStatusInfo`() {
-      // Given
-      // Creates referral and moves to awaiting allocation status
-      val awaitingAllocationStatus = referralStatusDescriptionRepository.getAwaitingAllocationStatusDescription()
-      val referral = testReferralHelper.createReferralAndUpdateStatus(awaitingAllocationStatus)
-
-      // When
-      val response = performRequestAndExpectOk(
-        httpMethod = HttpMethod.GET,
-        uri = "/referral/${referral.id}/status-change-details",
-        returnType = object : ParameterizedTypeReference<ReferralStatusInfo>() {},
-      )
-
-      // Then
-      assertThat(response).isNotNull
-      assertThat(response.newStatus).isEqualTo(ReferralStatusInfo.Status.AWAITING_ALLOCATION)
-      assertThat(response.sourcedFromEntityId).isEqualTo(referral.eventId!!.toLong())
-      assertThat(response.sourcedFromEntityType).isEqualTo(referral.sourcedFrom)
-      assertThat(response.notes).isNull()
     }
   }
 }
