4d27ca7c5d5276c71a37188aa706e60bb396dfde
Raby Whyte <raby.whyte@justice.gov.uk>
2026-04-09 09:35:41 +0100

    add updated at to programme group session slot (#552)
    * add updated at to programme group session slot flyway sql
    
    * entity definition to match flyway

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/entity/ProgrammeGroupSessionSlotEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/entity/ProgrammeGroupSessionSlotEntity.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.ent
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
@@ -9,12 +10,16 @@ import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotNull
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.DayOfWeek
+import java.time.LocalDateTime
 import java.time.LocalTime
 import java.util.UUID
 
 @Entity
 @Table(name = "programme_group_session_slot")
+@EntityListeners(AuditingEntityListener::class)
 class ProgrammeGroupSessionSlotEntity(
   @Id
   @GeneratedValue
@@ -33,4 +38,9 @@ class ProgrammeGroupSessionSlotEntity(
   @NotNull
   @Column(name = "start_time")
   var startTime: LocalTime,
+
+  @NotNull
+  @Column(name = "updated_at")
+  @LastModifiedDate
+  var updatedAt: LocalDateTime = LocalDateTime.now(),
 )
