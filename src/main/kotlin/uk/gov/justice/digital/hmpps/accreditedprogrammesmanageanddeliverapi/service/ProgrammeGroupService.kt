a657fc6b8149bb87cf561ec9041338826570fc91
nia evans <nia.evans@digital.justice.gov.uk>
2026-04-23 14:33:40 +0100

    Tidy up

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupService.kt
@@ -175,7 +175,7 @@ class ProgrammeGroupService(
         if (preGroupOneToOnePlaceholderSession == null) {
           log.warn("Pre-group one-to-one placeholder session not found for group $groupId when updating earliest start date. Cannot reschedule pre-group session.")
         } else {
-          log.info("Rescheduling pre-group one-to-one placeholder session ${preGroupOneToOnePlaceholderSession.id} for group $groupId to new date: ${it}")
+          log.info("Rescheduling pre-group one-to-one placeholder session ${preGroupOneToOnePlaceholderSession.id} for group $groupId to new date: $it")
 
           val rescheduleRequest = RescheduleSessionRequest(
             sessionStartDate = it,
