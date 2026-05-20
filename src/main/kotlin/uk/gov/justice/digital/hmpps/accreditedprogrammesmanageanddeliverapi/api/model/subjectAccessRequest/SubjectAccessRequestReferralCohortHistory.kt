2f53cac15ac849b0d177eba04fdcead5d9cde979
Ryan Forsyth <ryan.forsyth@justice.gov.uk>
2026-05-20 09:30:20 +0100

    Sar test and cohort additions (#657)
    * add more data to sar test for completeness. add cohort history to sar template
    
    * review fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/subjectAccessRequest/SubjectAccessRequestReferralCohortHistory.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/subjectAccessRequest/SubjectAccessRequestReferralCohortHistory.kt
@@ -1,17 +1,16 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.subjectAccessRequest
 
-import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.OffenceCohort
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralCohortHistoryEntity
 import java.time.LocalDateTime
 
 data class SubjectAccessRequestReferralCohortHistory(
   val createdBy: String?,
   val createdAt: LocalDateTime?,
-  val cohort: OffenceCohort,
+  val cohort: String?,
 )
 
 fun ReferralCohortHistoryEntity.toApi() = SubjectAccessRequestReferralCohortHistory(
   createdBy = createdBy,
   createdAt = createdAt,
-  cohort = cohort,
+  cohort = cohort.displayName,
 )
