72ef93e6322b1fa83da32ec75da24f537a87639b
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2026-05-27 11:19:49 +0100

    docs: add some inline docs to explain the onboarding controller

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/OnboardingController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/controller/OnboardingController.kt
@@ -30,6 +30,23 @@ data class FetchPersonalDetailsResponse(
   val failureIds: List<String>,
 )
 
+/**
+ * The OnboardingController was built to facilitate the onboarding of teams into M&D (away from
+ * IM) as part of the private beta in 2026-05.  It is not intended to be an extremely long-
+ * lived controller, past 2026, by which point all Regions in the UK (according to the plan
+ * at time of writing) should be using M&D.
+ *
+ * The Controller provides a set of utility endpoints for refreshing
+ * data about Referrals which may be cumbersome to do manually (e.g. clicking each Referral to
+ * update the PersonalDetails) - but where we don't yet have a fully automated approach (e.g. loading
+ * PersonalDetails as part of the IM Data Import process).  This is a pragmatic trade-off between
+ * what's "right" and what works, and will allow people to start using M&D in the real-world.
+ *
+ * As such: this is not precious code.  If you think we can do this in a better, more sustainable
+ * way elsewhere - I encourage the improvement of it.
+ *
+ * --TJWC 2026-05-27
+ */
 @RestController
 @PreAuthorize("hasAnyRole('ROLE_ACCREDITED_PROGRAMMES_MANAGE_AND_DELIVER_API__ACPMAD_UI_WR')")
 class OnboardingController(
