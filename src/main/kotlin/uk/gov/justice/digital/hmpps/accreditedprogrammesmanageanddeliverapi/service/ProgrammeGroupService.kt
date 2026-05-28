a7d2b685ce76c2da49d92e6d714aaca70f5a7ba5
Raby Whyte <raby.whyte@justice.gov.uk>
2026-05-28 12:10:15 +0100

    refactor to use the more idiomatic Kotlin style for empty list check

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupService.kt
@@ -434,34 +434,30 @@ class ProgrammeGroupService(
           groupId = group.id!!,
           sessionTemplateId = sessionTemplate.id!!,
         )?.filter { !it.isPlaceholder }
-        if (scheduledSessions.isNullOrEmpty()) {
-          emptyList()
-        } else {
-          scheduledSessions.map { scheduledSession ->
-            ProgrammeGroupModuleSessionsResponseGroupSession(
-              id = scheduledSession.id!!,
-              number = sessionTemplate.sessionNumber,
-              name = sessionNameFormatter.format(
-                scheduledSession,
-                SessionNameContext.SessionsAndAttendance(sessionTemplate),
-              ),
-              type = if (sessionTemplate.sessionType == SessionType.ONE_TO_ONE) "Individual" else "Group",
-              isCatchup = scheduledSession.isCatchup,
-              dateOfSession = scheduledSession.startsAt.toLocalDate(),
-              timeOfSession = formatTimeOfSession(
-                scheduledSession.startsAt.toLocalTime(),
-                scheduledSession.endsAt.toLocalTime(),
-              ),
-              participants = when {
-                sessionTemplate.sessionType == SessionType.GROUP && !scheduledSession.isCatchup -> listOf("All")
-                sessionTemplate.sessionType == SessionType.ONE_TO_ONE -> scheduledSession.attendees.map { it.personName }
-                else -> scheduledSession.attendees.map { it.personName }
-              },
-              facilitators = scheduledSession.sessionFacilitators.filter { it.facilitatorType != FacilitatorType.COVER_FACILITATOR }
-                .map { it.facilitator.personName },
-            )
-          }
-        }
+        scheduledSessions?.map { scheduledSession ->
+          ProgrammeGroupModuleSessionsResponseGroupSession(
+            id = scheduledSession.id!!,
+            number = sessionTemplate.sessionNumber,
+            name = sessionNameFormatter.format(
+              scheduledSession,
+              SessionNameContext.SessionsAndAttendance(sessionTemplate),
+            ),
+            type = if (sessionTemplate.sessionType == SessionType.ONE_TO_ONE) "Individual" else "Group",
+            isCatchup = scheduledSession.isCatchup,
+            dateOfSession = scheduledSession.startsAt.toLocalDate(),
+            timeOfSession = formatTimeOfSession(
+              scheduledSession.startsAt.toLocalTime(),
+              scheduledSession.endsAt.toLocalTime(),
+            ),
+            participants = when {
+              sessionTemplate.sessionType == SessionType.GROUP && !scheduledSession.isCatchup -> listOf("All")
+              sessionTemplate.sessionType == SessionType.ONE_TO_ONE -> scheduledSession.attendees.map { it.personName }
+              else -> scheduledSession.attendees.map { it.personName }
+            },
+            facilitators = scheduledSession.sessionFacilitators.filter { it.facilitatorType != FacilitatorType.COVER_FACILITATOR }
+              .map { it.facilitator.personName },
+          )
+        } ?: emptyList()
       }
 
       ProgrammeGroupModuleSessionsResponseGroupModule(
