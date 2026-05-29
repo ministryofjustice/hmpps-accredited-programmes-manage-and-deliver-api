ebdaa91ee4975740daee213202b5494253dec87b
Ryan Forsyth <ryan.forsyth@justice.gov.uk>
2026-05-29 11:29:29 +0100

    use helper function to only show last name of facilitators

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/subjectAccessRequest/SubjectAccessRequestFacilitator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/subjectAccessRequest/SubjectAccessRequestFacilitator.kt
@@ -1,11 +1,14 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.api.model.subjectAccessRequest
 
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.FacilitatorEntity
+import java.util.UUID
 
 data class SubjectAccessRequestFacilitator(
   val personName: String,
+  val id: UUID?,
 )
 
 fun FacilitatorEntity.toApi() = SubjectAccessRequestFacilitator(
   personName = personName,
+  id = id,
 )
