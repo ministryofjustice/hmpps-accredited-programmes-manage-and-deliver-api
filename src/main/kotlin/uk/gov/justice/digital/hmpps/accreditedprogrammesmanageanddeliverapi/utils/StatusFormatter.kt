f444c92e7d850f80167203e7bfbbecb3b5ae3a95
nia evans <nia.evans@digital.justice.gov.uk>
2026-06-16 11:56:19 +0100

    Linter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/utils/StatusFormatter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/utils/StatusFormatter.kt
@@ -1,10 +1,7 @@
 package uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.utils
 
 object StatusFormatter {
-  fun formatStatus(description: String): String =
-    if (description == "Breach (non-attendance)") "Breach" else description
+  fun formatStatus(description: String): String = if (description == "Breach (non-attendance)") "Breach" else description
 
-  fun unformatStatus(description: String?): String? =
-    if (description == "Breach") "Breach (non-attendance)" else description
+  fun unformatStatus(description: String?): String? = if (description == "Breach") "Breach (non-attendance)" else description
 }
-
