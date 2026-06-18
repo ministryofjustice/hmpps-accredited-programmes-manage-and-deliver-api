2440973ed04cc05fbcec240ff5f26f37e260fc37
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2026-06-18 11:16:08 +0100

    Filter nDelius appointment creation to future sessions only during referral allocation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupMembershipService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupMembershipService.kt
@@ -101,8 +101,15 @@ class ProgrammeGroupMembershipService(
       attendeeEntity
     }
 
-    // Create appointments in NDelius for each session object
-    scheduleService.createNdeliusAppointmentsForSessions(newAttendees)
+    // Create appointments in NDelius for each session object for future session only if they exist
+    val now = LocalDateTime.now(clock)
+    newAttendees
+      .filter { it.session.startsAt > now }
+      .takeIf { it.isNotEmpty() }
+      ?.let(scheduleService::createNdeliusAppointmentsForSessions)
+      ?: run {
+        log.info("Appointment NOT created in nDelius for past session on referral allocation for CRN: ${referral.crn} and group: ${group.code}")
+      }
 
     val savedReferral = referralRepository.save(referral)
 
