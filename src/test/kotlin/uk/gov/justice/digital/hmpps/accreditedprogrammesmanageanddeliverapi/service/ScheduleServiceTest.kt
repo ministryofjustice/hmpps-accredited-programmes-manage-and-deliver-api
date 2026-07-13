060e04772145768214e2c22b3397a713ef069268
Miroslav Živković <miroslav.zivkovic@justice.gov.uk>
2026-07-13 08:41:44 +0100

    [APG-2452] do not call nDelius if there are no session attendees (#791)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ScheduleServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ScheduleServiceTest.kt
@@ -6,10 +6,12 @@ import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatusCode
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.ClientResult
@@ -17,13 +19,21 @@ import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.clie
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.nDeliusIntegrationApi.model.CreateAppointmentRequest
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.client.nDeliusIntegrationApi.model.toAppointment
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.exception.BusinessException
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.common.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.config.logToAppInsights
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.AttendeeEntity
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ModuleSessionTemplateEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.NDeliusAppointmentEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ProgrammeGroupEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.ReferralEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.SessionEntity
 import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.entity.toNdeliusAppointmentEntity
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.factory.ScheduleSessionRequestFactory
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.ModuleSessionTemplateRepository
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.ProgrammeGroupRepository
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.ReferralRepository
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.repository.SessionRepository
+import uk.gov.justice.digital.hmpps.accreditedprogrammesmanageanddeliverapi.utils.SessionNameFormatter
 import java.util.UUID
 
 /**
@@ -39,20 +49,26 @@ class ScheduleServiceTest {
 
   private val nDeliusIntegrationApiClient = mockk<NDeliusIntegrationApiClient>()
   private val telemetryClient = mockk<TelemetryClient>(relaxed = true)
+  private val programmeGroupRepository = mockk<ProgrammeGroupRepository>(relaxed = true)
+  private val moduleSessionTemplateRepository = mockk<ModuleSessionTemplateRepository>(relaxed = true)
+  private val facilitatorService = mockk<FacilitatorService>(relaxed = true)
+  private val referralRepository = mockk<ReferralRepository>(relaxed = true)
+  private val sessionRepository = mockk<SessionRepository>(relaxed = true)
+  private val sessionNameFormatter = mockk<SessionNameFormatter>(relaxed = true)
 
   private val scheduleService = ScheduleService(
-    programmeGroupRepository = mockk(relaxed = true),
+    programmeGroupRepository = programmeGroupRepository,
     moduleRepository = mockk(relaxed = true),
     clock = mockk(relaxed = true),
     programmeGroupMembershipRepository = mockk(relaxed = true),
-    moduleSessionTemplateRepository = mockk(relaxed = true),
+    moduleSessionTemplateRepository = moduleSessionTemplateRepository,
     govUkApiClient = mockk(relaxed = true),
     nDeliusIntegrationApiClient = nDeliusIntegrationApiClient,
     nDeliusAppointmentRepository = mockk(relaxed = true),
-    facilitatorService = mockk(relaxed = true),
-    referralRepository = mockk(relaxed = true),
-    sessionRepository = mockk(relaxed = true),
-    sessionNameFormatter = mockk(relaxed = true),
+    facilitatorService = facilitatorService,
+    referralRepository = referralRepository,
+    sessionRepository = sessionRepository,
+    sessionNameFormatter = sessionNameFormatter,
     bankHolidayRepository = mockk(relaxed = true),
     telemetryClient = telemetryClient,
   )
@@ -64,7 +80,9 @@ class ScheduleServiceTest {
     // Function-reference form is refactor-safe (unlike the string "...Kt" form) - renaming
     // the enclosing file will fail at compile time, not silently at runtime.
     mockkStatic(AttendeeEntity::toAppointment, AttendeeEntity::toNdeliusAppointmentEntity)
-    every { any<AttendeeEntity>().toAppointment(any()) } returns mockk<CreateAppointmentRequest.NdeliusAppointment>(relaxed = true)
+    every { any<AttendeeEntity>().toAppointment(any()) } returns mockk<CreateAppointmentRequest.NdeliusAppointment>(
+      relaxed = true,
+    )
     every { any<AttendeeEntity>().toNdeliusAppointmentEntity(any()) } returns mockk<NDeliusAppointmentEntity>(relaxed = true)
   }
 
@@ -114,6 +132,125 @@ class ScheduleServiceTest {
     verify(exactly = 0) {
       telemetryClient.logToAppInsights("Appointment.create-nDelius.terminated-requirement", any())
     }
+  }
+
+  @Test
+  fun `scheduleIndividualSession successfully schedules a session`() {
+    // Given
+    val groupId = UUID.randomUUID()
+    val referralId = UUID.randomUUID()
+    val templateId = UUID.randomUUID()
+    val request = ScheduleSessionRequestFactory()
+      .withSessionTemplateId(templateId)
+      .withReferralIds(listOf(referralId))
+      .produce()
+
+    val programmeGroup = mockk<ProgrammeGroupEntity>(relaxed = true)
+    val sessionTemplate = mockk<ModuleSessionTemplateEntity>(relaxed = true)
+    val referral = mockk<ReferralEntity>(relaxed = true)
+
+    every { programmeGroupRepository.findByIdOrNull(groupId) } returns programmeGroup
+    every { moduleSessionTemplateRepository.findByIdOrNull(templateId) } returns sessionTemplate
+    every { referralRepository.findByIdOrNull(referralId) } returns referral
+    every { sessionRepository.save(any()) } answers { firstArg() }
+    every { nDeliusIntegrationApiClient.createAppointmentsInDelius(any()) } returns ClientResult.Success(
+      HttpStatusCode.valueOf(
+        200,
+      ),
+      mockk(relaxed = true),
+    )
+
+    // When
+    val result = scheduleService.scheduleIndividualSession(groupId, request)
+
+    // Then
+    verify(exactly = 1) { sessionRepository.save(any()) }
+    verify(exactly = 1) { nDeliusIntegrationApiClient.createAppointmentsInDelius(any()) }
+    assertThat(result.programmeGroup).isEqualTo(programmeGroup)
+    assertThat(result.moduleSessionTemplate).isEqualTo(sessionTemplate)
+    assertThat(result.attendees).hasSize(1)
+    assertThat(result.attendees.first().referral).isEqualTo(referral)
+  }
+
+  @Test
+  fun `scheduleIndividualSession throws NotFoundException when group is not found`() {
+    // Given
+    val groupId = UUID.randomUUID()
+    val request = ScheduleSessionRequestFactory()
+      .withSessionTemplateId(UUID.randomUUID())
+      .produce()
+
+    every { programmeGroupRepository.findByIdOrNull(groupId) } returns null
+
+    // When/Then
+    assertThatThrownBy { scheduleService.scheduleIndividualSession(groupId, request) }
+      .isInstanceOf(NotFoundException::class.java)
+      .hasMessage("Group with id: $groupId could not be found")
+  }
+
+  @Test
+  fun `scheduleIndividualSession throws NotFoundException when session template is not found`() {
+    // Given
+    val groupId = UUID.randomUUID()
+    val templateId = UUID.randomUUID()
+    val request = ScheduleSessionRequestFactory()
+      .withSessionTemplateId(templateId)
+      .produce()
+
+    every { programmeGroupRepository.findByIdOrNull(groupId) } returns mockk(relaxed = true)
+    every { moduleSessionTemplateRepository.findByIdOrNull(templateId) } returns null
+
+    // When/Then
+    assertThatThrownBy { scheduleService.scheduleIndividualSession(groupId, request) }
+      .isInstanceOf(NotFoundException::class.java)
+      .hasMessage("Session template with id: $templateId could not be found")
+  }
+
+  @Test
+  fun `scheduleIndividualSession throws NotFoundException when referral is not found`() {
+    // Given
+    val groupId = UUID.randomUUID()
+    val referralId = UUID.randomUUID()
+    val templateId = UUID.randomUUID()
+    val request = ScheduleSessionRequestFactory()
+      .withSessionTemplateId(templateId)
+      .withReferralIds(listOf(referralId))
+      .produce()
+
+    every { programmeGroupRepository.findByIdOrNull(groupId) } returns mockk(relaxed = true)
+    every { moduleSessionTemplateRepository.findByIdOrNull(templateId) } returns mockk(relaxed = true)
+    every { referralRepository.findByIdOrNull(referralId) } returns null
+
+    // When/Then
+    assertThatThrownBy { scheduleService.scheduleIndividualSession(groupId, request) }
+      .isInstanceOf(NotFoundException::class.java)
+      .hasMessage("Referral with id: $referralId could not be found")
+  }
+
+  @Test
+  fun `scheduleIndividualSession does not call nDelius when there are no attendees`() {
+    // Given
+    val groupId = UUID.randomUUID()
+    val templateId = UUID.randomUUID()
+    val request = ScheduleSessionRequestFactory()
+      .withSessionTemplateId(templateId)
+      .withReferralIds(listOf()) // No attendees
+      .produce()
+
+    val programmeGroup = mockk<ProgrammeGroupEntity>(relaxed = true)
+    val sessionTemplate = mockk<ModuleSessionTemplateEntity>(relaxed = true)
+
+    every { programmeGroupRepository.findByIdOrNull(groupId) } returns programmeGroup
+    every { moduleSessionTemplateRepository.findByIdOrNull(templateId) } returns sessionTemplate
+    every { sessionRepository.save(any()) } answers { firstArg() }
+
+    // When
+    val result = scheduleService.scheduleIndividualSession(groupId, request)
+
+    // Then
+    verify(exactly = 1) { sessionRepository.save(any()) }
+    verify(exactly = 0) { nDeliusIntegrationApiClient.createAppointmentsInDelius(any()) }
+    assertThat(result.attendees).isEmpty()
   }
 
   private fun buildAttendee(
