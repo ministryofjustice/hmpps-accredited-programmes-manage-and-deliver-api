56e8578aa03d9281a3ed0eb33b70ac85d2fb9940
pfcairns <phil.feroldi-cairns@digital.justice.gov.uk>
2026-08-03 16:27:44 +0100

    APG-2458 Prevent duplicate referrals by adding validation to `resolveReferralEventNumbers`.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ReferralEventNumberResolverService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ReferralEventNumberResolverService.kt
@@ -73,6 +73,10 @@ class ReferralEventNumberResolverService(
       it.subCategory?.code == LICENCE_CONDITION_BUILDING_CHOICES_SUBCATEGORY_CODE &&
         it.subCategory.description == BUILDING_CHOICES_SUBCATEGORY_DESCRIPTION
     }?.let {
+      if (duplicateReferralDetailsExists(referral, it.id.toString())) {
+        log.warn("Existing referral details found for crn: ${referral.crn}, event_id: $it.id.toString(), sourced_from: ${referral.sourcedFrom}")
+        return false
+      }
       referral.eventNumber = it.eventNumber.toInt()
       referral.eventId = it.id.toString()
       referralRepository.save(referral)
@@ -90,6 +94,10 @@ class ReferralEventNumberResolverService(
         it.subCategory.description == BUILDING_CHOICES_SUBCATEGORY_DESCRIPTION
     }
     buildingChoicesRequirement?.let {
+      if (duplicateReferralDetailsExists(referral, it.id.toString())) {
+        log.warn("Existing referral details found for crn: ${referral.crn}, event_id: $it.id.toString(), sourced_from: ${referral.sourcedFrom}")
+        return false
+      }
       referral.eventNumber = it.eventNumber.toInt()
       referral.eventId = it.id.toString()
       referralRepository.save(referral)
@@ -98,6 +106,16 @@ class ReferralEventNumberResolverService(
     } ?: logFailureEvent(referral)
     return false
   }
+
+  // A unique key on the referral table exists to prevent duplicate referrals being created for a given crn, event_id and sourced_from
+  fun duplicateReferralDetailsExists(
+    referral: ReferralEntity,
+    newEventId: String,
+  ): Boolean = referralRepository.existsByCrnAndEventIdAndSourcedFrom(
+    referral.crn,
+    newEventId,
+    referral.sourcedFrom!!,
+  )
 
   /**
    * When Referrals are populated by the data-importer-service, they have the eventNumber of 0, we know that this
