3e5f80d6e76efbdccefbdf53af696bb81162b22d
miroslav zivkovic <miroslav.zivkovic@justice.gov.uk>
2026-08-17 11:52:15 +0100

    [APG-2593] revert back changes

--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/ReferralTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/api/model/ReferralTest.kt
@@ -39,7 +39,7 @@ internal class ReferralTest {
     referralEntity.referralCohortHistories.add(cohortHistoryEntry)
 
     // Act
-    val referral = referralEntity.toApi(null, null)
+    val referral = referralEntity.toApi()
 
     // Assert
     assertEquals(id, referral.id)
@@ -79,7 +79,7 @@ internal class ReferralTest {
     referralEntity.referralCohortHistories.add(cohortHistoryEntry)
 
     // Act
-    val referral = referralEntity.toApi(null, null)
+    val referral = referralEntity.toApi()
 
     // Assert
     assertEquals(id, referral.id)
