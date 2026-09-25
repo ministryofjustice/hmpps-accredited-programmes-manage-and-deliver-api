551f870988ed5ef7710f6a03df0f9910eee236f0
Raby Whyte <raby.whyte@justice.gov.uk>
2026-09-25 12:05:05 +0100

    Revert "Fix: ProgrammeGroupServiceIntegrationTest - resolve NoSuchElementException in catch-up session test"
    This reverts commit 22bee7d773db3b7fb0c7e94be60ac3c10b18c902.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupServiceIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/accreditedprogrammesmanageanddeliverapi/service/ProgrammeGroupServiceIntegrationTest.kt
@@ -785,7 +785,7 @@ class ProgrammeGroupServiceIntegrationTest : IntegrationTestBase() {
       testDataGenerator.createSession(
         SessionFactory()
           .withProgrammeGroup(programmeGroup)
-          .withModuleSessionTemplate(regularModuleSessions.first())
+          .withModuleSessionTemplate(regularModuleSessions.first { it.sessionType == SessionType.GROUP })
           .withStartsAt(LocalDateTime.of(2026, 7, 20, 15, 30))
           .withEndsAt(LocalDateTime.of(2026, 7, 20, 17, 30))
           .withIsPlaceholder(false)
@@ -795,7 +795,7 @@ class ProgrammeGroupServiceIntegrationTest : IntegrationTestBase() {
       testDataGenerator.createSession(
         SessionFactory()
           .withProgrammeGroup(programmeGroup)
-          .withModuleSessionTemplate(regularModuleSessions.first())
+          .withModuleSessionTemplate(regularModuleSessions.first { it.sessionType == SessionType.GROUP })
           .withStartsAt(LocalDateTime.of(2026, 7, 27, 15, 30))
           .withEndsAt(LocalDateTime.of(2026, 7, 27, 17, 30))
           .withIsPlaceholder(false)
@@ -805,7 +805,7 @@ class ProgrammeGroupServiceIntegrationTest : IntegrationTestBase() {
       testDataGenerator.createSession(
         SessionFactory()
           .withProgrammeGroup(programmeGroup)
-          .withModuleSessionTemplate(gettingStartedModuleSessions.first())
+          .withModuleSessionTemplate(regularModuleSessions.first { it.sessionType == SessionType.ONE_TO_ONE })
           .withStartsAt(LocalDateTime.of(2026, 7, 27, 15, 30))
           .withEndsAt(LocalDateTime.of(2026, 7, 27, 17, 30))
           .withIsPlaceholder(false)
